dca543477991f3ec052a66f7ea0204c4a3b4b06a
Yaroslav <yaroslav@MacBook-Air-Aroslav.local>
2024-10-01 17:31:03 +0300

    Modified F.cpp

--- contest_0/F.cpp
+++ contest_0/F.cpp
@@ -1,14 +1,37 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
+
+const int cLim = 1e7;
+
+int BinarySearch(std::vector<std::vector<int> >& a,
+                 std::vector<std::vector<int> >& b, int i, int j, int l) {
+  int max = cLim;
+  int left = 0;
+  int right = l - 1;
+  int mid;
+  int answer = 0;
+  while (left <= right) {
+    mid = (left + right) / 2;
+    if (std::max(a[i][mid], b[j][mid]) < max) {
+      max = std::max(a[i][mid], b[j][mid]);
+      answer = mid + 1;
+    }
+    if (a[i][mid] < b[j][mid]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return answer;
+}
 
 int main() {
   int n;
   int m;
   int l;
   std::cin >> n >> m >> l;
-  std::vector<std::vector<int>> a(n, std::vector<int>(l, 0));
-  std::vector<std::vector<int>> b(m, std::vector<int>(l, 0));
+  std::vector<std::vector<int> > a(n, std::vector<int>(l, 0));
+  std::vector<std::vector<int> > b(m, std::vector<int>(l, 0));
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < l; j++) {
       std::cin >> a[i][j];
@@ -21,30 +44,13 @@ int main() {
   }
   int q;
   std::cin >> q;
-  const int cLim = 100000000;
   for (int w = 0; w < q; w++) {
     int i;
     int j;
     std::cin >> i >> j;
     i--;
     j--;
-    int answer = 0;
-    int maxim = cLim;
-    int left = 0;
-    int right = l - 1;
-    int mid;
-    while (left <= right) {
-      mid = (left + right) / 2;
-      if (std::max(a[i][mid], b[j][mid]) < maxim) {
-        maxim = std::max(a[i][mid], b[j][mid]);
-        answer = mid + 1;
-      }
-      if (a[i][mid] < b[j][mid]) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
-    }
-    std::cout << answer << "\n";
+    std::cout << BinarySearch(a, b, i, j, l) << "\n";
   }
+  return 0;
 }
