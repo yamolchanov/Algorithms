22e7030117e71d9da975cc221abdfe2d59d79f4a
Yaroslav <yaroslav@MacBook-Air-Aroslav.local>
2024-12-07 14:54:46 +0300

    Contest_3

--- contest_3/a.cpp
+++ contest_3/a.cpp
@@ -3,52 +3,54 @@
 #include <iostream>
 #include <vector>
 
-const int cLg = 18;
+struct SparseTable {
+  const int cLg = 18;
+  std::vector<std::vector<std::pair<int, int>>> table;
 
-std::pair<int, int> FindMinPair(const std::pair<int, int>& first,
-                                const std::pair<int, int>& second,
-                                const std::vector<int>& array) {
-  int indices[] = {first.first, first.second, second.first, second.second};
-  int min1_index = -1;
-  int min2_index = -1;
-  for (int i = 0; i < 4; ++i) {
-    int idx = indices[i];
-    if (idx == -1) {
-      continue;
+  static std::pair<int, int> FindMinPair(const std::pair<int, int>& first,
+                                         const std::pair<int, int>& second,
+                                         const std::vector<int>& array) {
+    int indices[] = {first.first, first.second, second.first, second.second};
+    int min1_index = -1;
+    int min2_index = -1;
+    for (int i = 0; i < 4; ++i) {
+      int idx = indices[i];
+      if (idx == -1) {
+        continue;
+      }
+      if (min1_index == -1 || array[idx] < array[min1_index]) {
+        min2_index = min1_index;
+        min1_index = idx;
+      } else if ((min2_index == -1 || array[idx] < array[min2_index]) &&
+                 idx != min1_index) {
+        min2_index = idx;
+      }
     }
-    if (min1_index == -1 || array[idx] < array[min1_index]) {
-      min2_index = min1_index;
-      min1_index = idx;
-    } else if ((min2_index == -1 || array[idx] < array[min2_index]) &&
-               idx != min1_index) {
-      min2_index = idx;
+    return {min1_index, min2_index};
+  }
+
+  SparseTable(const int cN, const std::vector<int>& array) {
+    table.resize(cN, std::vector<std::pair<int, int>>(cLg));
+    for (int i = 0; i < cN; ++i) {
+      table[i][0] = {i, -1};
+    }
+    for (int j = 1; j < cLg; ++j) {
+      for (int i = 0; i + (1 << j) <= cN; ++i) {
+        std::pair<int, int> left = table[i][j - 1];
+        std::pair<int, int> right = table[i + (1 << (j - 1))][j - 1];
+        table[i][j] = FindMinPair(left, right, array);
+      }
     }
   }
-  return {min1_index, min2_index};
-}
 
-void BuildSparse(int n, std::vector<std::vector<std::pair<int, int>>>& table,
-                 const std::vector<int>& array) {
-  for (int i = 0; i < n; ++i) {
-    table[i][0] = {i, -1};
+  std::pair<int, int> Answer(const int cL, const int cR,
+                             const std::vector<int>& array) const {
+    int j = static_cast<int>(std::log2(cR - cL + 1));
+    std::pair<int, int> left = table[cL][j];
+    std::pair<int, int> right = table[cR - (1 << j) + 1][j];
+    return FindMinPair(left, right, array);
   }
-  for (int j = 1; j < cLg; ++j) {
-    for (int i = 0; i + (1 << j) <= n; ++i) {
-      std::pair<int, int> left = table[i][j - 1];
-      std::pair<int, int> right = table[i + (1 << (j - 1))][j - 1];
-      table[i][j] = FindMinPair(left, right, array);
-    }
-  }
-}
-
-std::pair<int, int> Answer(
-    int l, int r, const std::vector<std::vector<std::pair<int, int>>>& table,
-    const std::vector<int>& array) {
-  int j = std::log2(r - l + 1);
-  std::pair<int, int> left = table[l][j];
-  std::pair<int, int> right = table[r - (1 << j) + 1][j];
-  return FindMinPair(left, right, array);
-}
+};
 
 int main() {
   int n;
@@ -58,16 +60,16 @@ int main() {
   for (int i = 0; i < n; ++i) {
     std::cin >> array[i];
   }
-  std::vector<std::vector<std::pair<int, int>>> table(
-      n, std::vector<std::pair<int, int>>(cLg));
-  BuildSparse(n, table, array);
-  int l;
-  int r;
+
+  SparseTable st(n, array);
+
   for (int i = 0; i < q; ++i) {
+    int l;
+    int r;
     std::cin >> l >> r;
     --l;
     --r;
-    std::pair<int, int> result = Answer(l, r, table, array);
+    std::pair<int, int> result = st.Answer(l, r, array);
     std::cout << array[result.second] << "\n";
   }
 }
