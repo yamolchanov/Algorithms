74fcae2dbcc2a68f38bcd340e1a06f7a2e3a745f
Yaroslav <molchanov.iae@phystech.edu>
2025-03-09 17:27:49 +0300

    Contest_6

---
+++ contest_6/K.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  int n;
+  int length = 0;
+  std::cin >> n;
+  std::vector<int> a(n);
+  std::vector last(n, -1);
+  std::vector indexs(n + 1, -1);
+  for (int i = 0; i != n; ++i) {
+    std::cin >> a[i];
+  }
+
+  for (int i = 0; i < n; i++) {
+    int low = 1;
+    int high = length;
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      if (a[indexs[mid]] >= a[i]) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+
+    int pos = low;
+    last[i] = indexs[pos - 1];
+    indexs[pos] = i;
+    length = std::max(pos, length);
+  }
+
+  std::cout << length << "\n";
+
+  std::vector<int> result(length);
+  int k = indexs[length];
+  for (int i = length - 1; i >= 0; --i) {
+    result[i] = k + 1;
+    k = last[k];
+  }
+
+  for (int i = 0; i < length; i++) {
+    std::cout << result[i] << " ";
+  }
+}
