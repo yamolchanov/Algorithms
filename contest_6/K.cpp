8ea3e25719c979fcbf87d9fff9de7bdb45a9c2fd
Yaroslav <molchanov.iae@phystech.edu>
2025-03-17 12:13:52 +0300

    Moved the logic out of main in K.cpp

--- contest_6/K.cpp
+++ contest_6/K.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
 #include <vector>
 
-int main() {
-  int n;
-  int length = 0;
-  std::cin >> n;
-  std::vector<int> a(n);
-  std::vector last(n, -1);
-  std::vector indexs(n + 1, -1);
-  for (int i = 0; i != n; ++i) {
-    std::cin >> a[i];
-  }
-
+void Logic(int n, std::vector<int>& indexs, int& length, std::vector<int>& last,
+           std::vector<int>& a) {
   for (int i = 0; i < n; i++) {
     int low = 1;
     int high = length;
@@ -29,17 +20,32 @@ int main() {
     indexs[pos] = i;
     length = std::max(pos, length);
   }
-
   std::cout << length << "\n";
+}
 
-  std::vector<int> result(length);
-  int k = indexs[length];
+void Table(int length, std::vector<int>& result, int k,
+           std::vector<int>& last) {
   for (int i = length - 1; i >= 0; --i) {
     result[i] = k + 1;
     k = last[k];
   }
-
   for (int i = 0; i < length; i++) {
     std::cout << result[i] << " ";
   }
 }
+
+int main() {
+  int n;
+  int length = 0;
+  std::cin >> n;
+  std::vector<int> a(n);
+  std::vector last(n, -1);
+  std::vector indexs(n + 1, -1);
+  for (int i = 0; i != n; ++i) {
+    std::cin >> a[i];
+  }
+  Logic(n, indexs, length, last, a);
+  std::vector<int> result(length);
+  Table(length, result, indexs[length], last);
+}
+
