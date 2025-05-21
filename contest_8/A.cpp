b0a26285dd8bf38625a61900981b2fa3f9106556
Yaroslav <molchanov.iae@phystech.edu>
2025-05-21 15:17:21 +0300

    fix

--- contest_8/A.cpp
+++ contest_8/A.cpp
@@ -1,44 +1,30 @@
 #include <iostream>
 #include <set>
 #include <vector>
+
 const long long cNot = 2009000999;
-int main() {
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(nullptr);
-  int t;
-  int n;
-  int m;
-  int s;
-  std::cin >> t;
-  for (int z = 0; z < t; z++) {
-    std::cin >> n >> m;
-    std::vector<std::vector<std::pair<int, int>>> g(n);
-    for (int i = 0; i < m; i++) {
-      int x;
-      int y;
-      int w;
-      std::cin >> x >> y >> w;
-      g[x].push_back({y, w});
-      g[y].push_back({x, w});
-    }
-    std::cin >> s;
-    std::vector<int> d(n, cNot);
-    std::vector<int> used(n, 0);
+
+class Graph {
+public:
+  Graph(int n) : n_(n), g_(n) {}
+
+  void AddEdge(int x, int y, int w) {
+    g_[x].emplace_back(y, w);
+    g_[y].emplace_back(x, w);
+  }
+
+  std::vector<int> Dijkstra(int s) {
+    std::vector<int> d(n_, cNot);
     std::set<std::pair<int, int>> q;
     d[s] = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n_; ++i) {
       q.insert({d[i], i});
     }
-    for (int i = 0; i < n; i++) {
+    while (!q.empty()) {
       int v = q.begin()->second;
       q.erase(q.begin());
-      if (d[v] >= cNot) {
-        break;
-      }
-      used[v] = 1;
-      for (int j = 0; j < static_cast<int>(g[v].size()); j++) {
-        int to = g[v][j].first;
-        int len = g[v][j].second;
+
+      for (auto& [to, len] : g_[v]) {
         if (len != -1 && d[v] + len < d[to]) {
           q.erase({d[to], to});
           d[to] = d[v] + len;
@@ -46,8 +32,42 @@ int main() {
         }
       }
     }
-    for (int u : d) {
-      std::cout << u << " ";
-    }
+    return d;
   }
+
+private:
+  int n_;
+  std::vector<std::vector<std::pair<int, int>>> g_;
+};
+
+void Solve() {
+  int t;
+  std::cin >> t;
+  while (t > 0) {
+    int n;
+    int m;
+    int s;
+    std::cin >> n >> m;
+    Graph graph(n);
+    for (int i = 0; i < m; ++i) {
+      int x;
+      int y;
+      int w;
+      std::cin >> x >> y >> w;
+      graph.AddEdge(x, y, w);
+    }
+    std::cin >> s;
+    std::vector<int> distances = graph.Dijkstra(s);
+    for (int dist : distances) {
+      std::cout << dist << " ";
+    }
+    std::cout << "\n";
+    t--;
+  }
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  Solve();
 }
--- contest_8/M.cpp
+++ contest_8/M.cpp
@@ -2,62 +2,91 @@
 #include <iostream>
 #include <vector>
 
-struct DSU {
-  std::vector<int> parent;
-  std::vector<int> rank;
+class DSU {
+ private:
+  std::vector<int> parent_;
+  std::vector<int> rank_;
 
-  DSU(int n) : parent(n), rank(n, 1) {
+ public:
+  DSU(int n) : parent_(n), rank_(n, 1) {
     for (int i = 0; i < n; ++i) {
-      parent[i] = i;
+      parent_[i] = i;
     }
   }
 
-  int Evristika(int u) {
-    while (u != parent[u]) {
-      u = parent[u];
+  int Find(int u) {
+    while (u != parent_[u]) {
+      u = parent_[u];
     }
     return u;
   }
 
-  bool Conn(int u, int v) {
-    u = Evristika(u);
-    v = Evristika(v);
+  bool Unite(int u, int v) {
+    u = Find(u);
+    v = Find(v);
     if (u == v) {
       return false;
     }
-    if (rank[u] < rank[v]) {
+    if (rank_[u] < rank_[v]) {
       std::swap(u, v);
     }
-    parent[v] = u;
-    if (rank[u] == rank[v]) {
-      rank[u]++;
+    parent_[v] = u;
+    if (rank_[u] == rank_[v]) {
+      rank_[u]++;
     }
     return true;
   }
 };
 
-struct Edge {
-  int u;
-  int v;
-  int c;
+class Graph {
+ private:
+  struct Edge {
+    int u;
+    int v;
+    int weight;
+
+    Edge(int first, int second, int w) : u(first), v(second), weight(w) {}
+
+    static bool CompareEdges(const Edge& a, const Edge& b) {
+      return a.weight < b.weight;
+    }
+  };
+
+  int vertex_count_;
+  std::vector<Edge> edges_;
+
+ public:
+  Graph(int n) : vertex_count_(n) {}
+  void AddEdge(int u, int v, int weight) {
+    edges_.emplace_back(u - 1, v - 1, weight);
+  }
+
+  long long FindMST() {
+    std::sort(edges_.begin(), edges_.end(), Edge::CompareEdges);
+    DSU dsu(vertex_count_);
+    long long total_weight = 0;
+    for (const auto& edge : edges_) {
+      if (dsu.Unite(edge.u, edge.v)) {
+        total_weight += edge.weight;
+      }
+    }
+    return total_weight;
+  }
 };
 
-int main() {
+void Solve() {
   int n;
   int m;
   std::cin >> n >> m;
-  std::vector<Edge> edges(m);
-  for (auto& e : edges) {
-    std::cin >> e.u >> e.v >> e.c;
+  Graph graph(n);
+  for (int i = 0; i < m; ++i) {
+    int u;
+    int v;
+    int c;
+    std::cin >> u >> v >> c;
+    graph.AddEdge(u, v, c);
   }
-  std::sort(edges.begin(), edges.end(),
-            [](const Edge& a, const Edge& b) { return a.c < b.c; });
-  DSU dsu(n);
-  long long weight = 0;
-  for (auto& e : edges) {
-    if (dsu.Conn(e.u - 1, e.v - 1)) {
-      weight += e.c;
-    }
-  }
-  std::cout << weight << '\n';
+  std::cout << graph.FindMST() << '\n';
 }
+
+int main() { Solve(); }
