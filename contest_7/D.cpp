80422057a87545998a4c76a64062000cb02662ca
Yaroslav <molchanov.iae@phystech.edu>
2025-05-22 23:36:32 +0300

    fix

--- contest_7/D.cpp
+++ contest_7/D.cpp
@@ -3,102 +3,102 @@
 #include <string>
 #include <vector>
 
-class Shell {
- public:
-  std::vector<std::string> was;
-  std::vector<int> order;
-  std::vector<int> comp;
-  std::vector<int> ans;
-
-  Shell(int n) : was(n, "white"), ans(n, 0) {}
-
-  void ResetWas() { std::ranges::fill(was, "white"); }
-
-  void ClearComp() { comp.clear(); }
-};
-
 class Graph {
- public:
-  int n;
-  std::vector<std::vector<int>> e;
-  std::vector<std::vector<int>> er;
+public:
+    int n;
+    std::vector<std::vector<int>> e;
+    std::vector<std::vector<int>> er;
 
-  Graph(int n, int m) : n(n), e(n), er(n) {
-    for (int i = 0; i < m; ++i) {
-      int a;
-      int b;
-      std::cin >> a >> b;
-      a--;
-      b--;
-      AddEdge(a, b);
+    Graph(int n, int m) : n(n), e(n), er(n) {
+        for (int i = 0; i < m; ++i) {
+            int a;
+            int b;
+            std::cin >> a >> b;
+            --a;
+            --b;
+            AddEdge(a, b);
+        }
     }
-  }
 
-  void AddEdge(int a, int b) {
-    e[a].push_back(b);
-    er[b].push_back(a);
-  }
+    void AddEdge(int a, int b) {
+        e[a].push_back(b);
+        er[b].push_back(a);
+    }
 };
 
-void DFS1(int v, const Graph& g, Shell& shell) {
-  shell.was[v] = "grey";
-  for (int u : g.e[v]) {
-    if (shell.was[u] == "white") {
-      DFS1(u, g, shell);
+class Shell {
+public:
+    std::vector<std::string> was;
+    std::vector<int> order;
+    std::vector<int> comp;
+    std::vector<int> ans;
+
+    Shell(int n) : was(n, "white"), ans(n, 0) {}
+
+    void ResetWas() { std::ranges::fill(was, "white"); }
+    void ClearComp() { comp.clear(); }
+
+    void Solve(const Graph& g) {
+        int cnt = 0;
+
+        for (int i = 0; i < g.n; ++i) {
+            if (was[i] == "white") {
+                DFS1(i, g);
+            }
+        }
+
+        ResetWas();
+        std::ranges::reverse(order);
+
+        for (int v : order) {
+            if (was[v] == "white") {
+                DFS2(v, g, cnt);
+                ++cnt;
+                ClearComp();
+            }
+        }
+
+        std::cout << cnt << "\n";
+        for (int x : ans) {
+            std::cout << x + 1 << " ";
+        }
+        std::cout << "\n";
     }
-  }
-  shell.was[v] = "black";
-  shell.order.push_back(v);
-}
 
-void DFS2(int v, const Graph& g, Shell& shell, int cnt) {
-  shell.was[v] = "grey";
-  shell.comp.push_back(v);
-  shell.ans[v] = cnt;
-  for (int u : g.er[v]) {
-    if (shell.was[u] == "white") {
-      DFS2(u, g, shell, cnt);
+private:
+    void DFS1(int v, const Graph& g) {
+        was[v] = "grey";
+        for (int u : g.e[v]) {
+            if (was[u] == "white") {
+                DFS1(u, g);
+            }
+        }
+        was[v] = "black";
+        order.push_back(v);
     }
-  }
-  shell.was[v] = "black";
-}
 
-void Solve(const Graph& g, Shell& shell) {
-  int cnt = 0;
-
-  for (int i = 0; i < g.n; ++i) {
-    if (shell.was[i] == "white") {
-      DFS1(i, g, shell);
+    void DFS2(int v, const Graph& g, int cnt) {
+        was[v] = "grey";
+        comp.push_back(v);
+        ans[v] = cnt;
+        for (int u : g.er[v]) {
+            if (was[u] == "white") {
+                DFS2(u, g, cnt);
+            }
+        }
+        was[v] = "black";
     }
-  }
-
-  shell.ResetWas();
-  std::ranges::reverse(shell.order);
-
-  for (int v : shell.order) {
-    if (shell.was[v] == "white") {
-      DFS2(v, g, shell, cnt);
-      ++cnt;
-      shell.ClearComp();
-    }
-  }
-
-  std::cout << cnt << "\n";
-  for (int x : shell.ans) {
-    std::cout << x + 1 << " ";
-  }
-  std::cout << "\n";
-}
+};
 
 int main() {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
 
-  int n;
-  int m;
-  std::cin >> n >> m;
+    int n;
+    int m;
+    std::cin >> n >> m;
 
-  Graph g(n, m);
-  Shell shell(n);
-  Solve(g, shell);
+    Graph g(n, m);
+    Shell shell(n);
+    shell.Solve(g);
 }
--- contest_7/E.cpp
+++ contest_7/E.cpp
@@ -3,89 +3,91 @@
 #include <vector>
 
 struct GraphShell {
-  std::vector<int> tin;
-  std::vector<int> fup;
-  std::vector<std::string> color;
-  std::vector<int> bridges;
+    std::vector<int> tin;
+    std::vector<int> fup;
+    std::vector<std::string> color;
+    std::vector<int> bridges;
 
-  explicit GraphShell(int n) {
-    tin.resize(n + 1, -1);
-    fup.resize(n + 1, -1);
-    color.resize(n + 1, "WHITE");
-  }
+    explicit GraphShell(int n) {
+        tin.resize(n + 1, -1);
+        fup.resize(n + 1, -1);
+        color.resize(n + 1, "WHITE");
+    }
 };
 
 struct Graph {
 private:
-  int n_;
-  int timer_;
-  std::vector<std::vector<std::pair<int, int>>> adj_;
-  GraphShell shell_;
+    int n_;
+    int timer_;
+    std::vector<std::vector<std::pair<int, int>>> adj_;
+    GraphShell shell_;
 
 public:
-  explicit Graph(int nodes) : n_(nodes), timer_(0), shell_(nodes) {
-    adj_.resize(n_ + 1);
-  }
-
-  void AddEdge(int u, int v, int id) {
-    if (u != v) {
-      adj_[u].push_back({v, id});
-      adj_[v].push_back({u, id});
+    explicit Graph(int nodes) : n_(nodes), timer_(0), shell_(nodes) {
+        adj_.resize(n_ + 1);
     }
-  }
 
-  void Solve() {
-    FindBridge();
-    std::cout << shell_.bridges.size() << "\n";
-    std::sort(shell_.bridges.begin(), shell_.bridges.end());
-    for (int bridge : shell_.bridges) {
-      std::cout << bridge << " ";
+    void AddEdge(int u, int v, int id) {
+        if (u != v) {
+            adj_[u].push_back({v, id});
+            adj_[v].push_back({u, id});
+        }
     }
-    std::cout << "\n";
-  }
+
+    void Solve() {
+        FindBridge();
+        std::cout << shell_.bridges.size() << "\n";
+        std::sort(shell_.bridges.begin(), shell_.bridges.end());
+        for (int bridge : shell_.bridges) {
+            std::cout << bridge << " ";
+        }
+        std::cout << "\n";
+    }
 
 private:
-  void DFS(int v, int p_edge_id = -1) {
-    shell_.color[v] = "GREY";
-    shell_.tin[v] = timer_;
-    shell_.fup[v] = timer_;
-    timer_++;
-    for (auto [to, edge_id] : adj_[v]) {
-      if (edge_id == p_edge_id) continue;
-      if (shell_.color[to] == "GREY") {
-        shell_.fup[v] = std::min(shell_.fup[v], shell_.tin[to]);
-      } else if (shell_.color[to] == "WHITE") {
-        DFS(to, edge_id);
-        shell_.fup[v] = std::min(shell_.fup[v], shell_.fup[to]);
-        if (shell_.fup[to] > shell_.tin[v]) {
-          shell_.bridges.push_back(edge_id);
+    void DFS(int v, int p_edge_id = -1) {
+        shell_.color[v] = "GREY";
+        shell_.tin[v] = timer_;
+        shell_.fup[v] = timer_;
+        timer_++;
+        for (auto [to, edge_id] : adj_[v]) {
+            if (edge_id == p_edge_id) {
+                continue;
+            }
+            if (shell_.color[to] == "GREY") {
+                shell_.fup[v] = std::min(shell_.fup[v], shell_.tin[to]);
+            } else if (shell_.color[to] == "WHITE") {
+                DFS(to, edge_id);
+                shell_.fup[v] = std::min(shell_.fup[v], shell_.fup[to]);
+                if (shell_.fup[to] > shell_.tin[v]) {
+                    shell_.bridges.push_back(edge_id);
+                }
+            }
         }
-      }
+        shell_.color[v] = "BLACK";
     }
-    shell_.color[v] = "BLACK";
-  }
 
-  void FindBridge() {
-    for (int i = 1; i <= n_; ++i) {
-      if (shell_.color[i] == "WHITE") {
-        DFS(i);
-      }
+    void FindBridge() {
+        for (int i = 1; i <= n_; ++i) {
+            if (shell_.color[i] == "WHITE") {
+                DFS(i);
+            }
+        }
     }
-  }
 };
 
 void Solve() {
-  int n, m;
-  std::cin >> n >> m;
-  Graph graph(n);
-  for (int i = 1; i <= m; ++i) {
-    int u, v;
-    std::cin >> u >> v;
-    graph.AddEdge(u, v, i);
-  }
-  graph.Solve();
+    int n;
+    int m;
+    std::cin >> n >> m;
+    Graph graph(n);
+    for (int i = 1; i <= m; ++i) {
+        int v;
+        int u;
+        std::cin >> u >> v;
+        graph.AddEdge(u, v, i);
+    }
+    graph.Solve();
 }
 
-int main() {
-  Solve();
-}
+int main() { Solve(); }
