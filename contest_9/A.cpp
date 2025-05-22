49c3940f1751891f4ce5d88cc40f3785e2ec8d84
Yaroslav <molchanov.iae@phystech.edu>
2025-05-23 00:29:05 +0300

    A.cpp

--- contest_9/A.cpp
+++ contest_9/A.cpp
@@ -9,52 +9,75 @@ struct Graph {
   void AddEdge(int u, int v) { adj[u].push_back(v); }
 };
 
-bool TryKuhn(int v, const Graph& g, std::vector<bool>& used,
-             std::vector<int>& mt) {
-  if (used[v]) {
+class Shell {
+public:
+  void Run() {
+    ReadInput();
+    Kuhn();
+    Result();
+  }
+
+private:
+  int n_;
+  int k_;
+  Graph g_{0, 0};
+  std::vector<int> mt_;
+  std::vector<bool> used_;
+
+  void ReadInput() {
+    std::cin >> n_ >> k_;
+    g_ = Graph(n_, k_);
+    mt_.assign(k_, -1);
+    used_.assign(n_, false);
+
+    for (int i = 0; i < n_; ++i) {
+      int j;
+      std::cin >> j;
+      while (j != 0) {
+        g_.AddEdge(i, j - 1);
+        std::cin >> j;
+      }
+    }
+  }
+
+  void Kuhn() {
+    for (int v = 0; v < n_; ++v) {
+      used_.assign(n_, false);
+      TryKuhn(v);
+    }
+  }
+
+  bool TryKuhn(int v) {
+    if (used_[v]) {
+      return false;
+    }
+    used_[v] = true;
+    for (int to : g_.adj[v]) {
+      if (mt_[to] == -1 || TryKuhn(mt_[to])) {
+        mt_[to] = v;
+        return true;
+      }
+    }
     return false;
   }
-  used[v] = true;
-  for (int to : g.adj[v]) {
-    if (mt[to] == -1 || TryKuhn(mt[to], g, used, mt)) {
-      mt[to] = v;
-      return true;
-    }
-  }
-  return false;
-}
 
-void Solve() {
-  int n;
-  int k;
-  std::cin >> n >> k;
-  int count = 0;
-  Graph g(n, k);
-  std::vector<int> mt(k, -1);
-  std::vector<bool> used(n);
-  for (int i = 0; i < n; ++i) {
-    int j;
-    std::cin >> j;
-    while (j != 0) {
-      g.AddEdge(i, j - 1);
-      std::cin >> j;
+  void Result() {
+    int count = 0;
+    for (int i = 0; i < k_; ++i) {
+      if (mt_[i] != -1) {
+        ++count;
+      }
+    }
+    std::cout << count << "\n";
+    for (int i = 0; i < k_; ++i) {
+      if (mt_[i] != -1) {
+        std::cout << mt_[i] + 1 << " " << i + 1 << "\n";
+      }
     }
   }
-  for (int v = 0; v < n; ++v) {
-    used.assign(n, false);
-    TryKuhn(v, g, used, mt);
-  }
-  for (int i = 0; i < k; ++i) {
-    if (mt[i] != -1) {
-      ++count;
-    }
-  }
-  std::cout << count << "\n";
-  for (int i = 0; i < k; ++i) {
-    if (mt[i] != -1) {
-      std::cout << mt[i] + 1 << " " << i + 1 << "\n";
-    }
-  }
-}
+};
 
-int main() { Solve(); }
+int main() {
+  Shell shell;
+  shell.Run();
+}
